0f0df38d6b626a6a2ff4a5417a17406595970ec0
robert-orri <robertorriarnason@gmail.com>
2023-05-05 14:31:20 +0000

    test

--- manufacturer/views.py
+++ manufacturer/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 # Create your views here.
+
+offers = [
+    {'name': '2 for 1', 'price': '4500'},
+    {'name': 'Ghost Challenge', 'price': '3500'},
+    {'name': 'Family offer', 'price': '6000'}
+]
 def index(request):
-    return render(request, 'manufacturer/index.html')
+    return render(request, 'ghost/index.html', context={
+        'offers': offers
+    })

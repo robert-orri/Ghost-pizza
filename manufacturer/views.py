ea4badac3a198c55f7587c9a6dc7898d7d6544cf
robert-orri <robertorriarnason@gmail.com>
2023-05-06 13:57:35 +0000

    New ting

--- manufacturer/views.py
+++ manufacturer/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-# Create your views here.
 
 offers = [
     {'name': '2 for 1', 'price': '4500'},
     {'name': 'Ghost Challenge', 'price': '3500'},
     {'name': 'Family offer', 'price': '6000'}
+
 ]
+
+# Create your views here.
 def index(request):
     return render(request, 'ghost/index.html', context={
         'offers': offers

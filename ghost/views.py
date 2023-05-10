2240184062a8840475d98430e8f592d2bf82a68f
robert-orri <robertorriarnason@gmail.com>
2023-05-10 22:28:35 +0000

    New ting

--- ghost/views.py
+++ ghost/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from ghost.models import Pizza
 
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'ghost/index.html', {
         'Pizzas': Pizza.objects.all()
